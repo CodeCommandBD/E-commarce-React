b9d96dc953312f12710246d0e79846aebddf3388
CodeCommandBD <shantokumar22@gmail.com>
2025-06-01 23:02:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Products from './Pages/Products'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Cart from './Pages/Cart'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import axios from 'axios'
+
+const App = () => {
+  const [location, setLocation] = useState(null)
+  const getLocation = async () => {
+    navigator.geolocation.getCurrentPosition(async pos => {
+      const { latitude, longitude } = pos.coords
+
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
+      try {
+        const location = await axios.get(url)
+        const exactLocation = location.data.address
+        setLocation(exactLocation)
+      } catch (error) {
+        console.log(error);
+
+      }
+    })
+
+
+  }
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar location={location}></Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>} ></Route>
+          <Route path='/products' element={<Products></Products>} ></Route>
+          <Route path='/about' element={<About></About>} ></Route>
+          <Route path='/contact' element={<Contact></Contact>} ></Route>
+          <Route path='/cart' element={<Cart></Cart>} ></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { CiLocationOn } from "react-icons/ci";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { BsCart3 } from "react-icons/bs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+
+const Navbar = ({location}) => {
+    
+    return (
+        <div>
+            <div className='bg-white py-3 shadow-2xl'>
+                <div className='container flex items-center justify-between'>
+
+                    {/* logo section */}
+                    <div className='flex items-center gap-5'>
+                        <div className="logo ">
+                            <Link to='/'>
+                                <img className='w-[80px] h-[35px]' src="/e-Logo.jpg" alt="" />
+                            </Link>
+                        </div>
+
+                        {/* location section */}
+                        
+                        <div className='flex items-center justify-center '>
+                            <CiLocationOn className='text-purple-800 font-bold text-xl' />
+
+                            <span className='font-semibold'>{location ? <div>{location.city}, {location.country}</div> : <h3 className='text-purple-800'>Add Location</h3>}
+                            </span>
+                            <IoMdArrowDropdown className='text-purple-800' />
+                        </div>
+                    </div>
+
+                    {/* menu section */}
+                    <nav className='flex gap-7'>
+                        <ul className='flex items-center gap-5 font-semibold text-lg'>
+                            <NavLink to='/' className={({ isActive }) => `${isActive ? "border-b-3 transition-all text-purple-800 border-purple-800" : "text-black"} cursor-pointer`} ><li>Home</li></NavLink>
+
+                            <NavLink to='/products' className={({ isActive }) => `${isActive ? "border-b-3 transition-all text-purple-800  border-purple-800" : "text-black"} cursor-pointer`}><li>Products</li></NavLink>
+
+                            <NavLink to='/about' className={({ isActive }) => `${isActive ? "border-b-3 transition-all text-purple-800  border-purple-800" : "text-black"} cursor-pointer`}><li>About</li></NavLink>
+
+                            <NavLink to='/contact' className={({ isActive }) => `${isActive ? "border-b-3 transition-all text-purple-800  border-purple-800" : "text-black"} cursor-pointer`}><li>Contact</li></NavLink>
+
+                        </ul>
+
+                        {/* cart section */}
+                        <div className='flex items-center gap-5'>
+                            <Link to='/cart'>
+                                <div className='cart relative hover:text-purple-800'>
+                                    <BsCart3 className='h-7 w-7' />
+                                    <span className='absolute bg-red-700 px-1.5 rounded-full text-white -top-2 -end-3'>5</span>
+                                </div>
+                            </Link>
+                           
+                        </div>
+                        {/* sign in and sign out section */}
+                        <div>
+                            <SignedOut >
+                                <SignInButton className='text-lg py-1.5 px-3.5 rounded-xl bg-purple-800 text-white cursor-pointer' />
+                            </SignedOut>
+                            <SignedIn >
+                                <UserButton className='text-lg py-1.5 px-3.5 rounded-xl bg-purple-800 text-white cursor-pointer' />
+                            </SignedIn>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
